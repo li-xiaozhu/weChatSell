c9d1f2b8917b85946961889b9a2896948fc2a42d
zhangqingqing <121839154@qq.com>
2020-03-09 19:38:30 +0800

    底部导航文案修改

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -3,8 +3,8 @@
         <li class="l1" :class="active==1?'active':''" @click="goPatth('/equipment')">
             <span></span>我的设备 <p :class="isAlarm==0?'':'isAlarm'"></p>
         </li>
-        <li class="l2" :class="active==2?'active':''" @click="goPatth('/service')"><span></span>服务中心</li>
-        <li class="l3" :class="active==3?'active':''" @click="goPatth('/repair')"><span></span>在线报修</li>
+        <li class="l2" :class="active==2?'active':''" @click="goPatth('/service')"><span></span>产品应用</li>
+        <li class="l3" :class="active==3?'active':''" @click="goPatth('/repair')"><span></span>故障报修</li>
         <li class="l4" :class="active==4?'active':''" @click="goPatth('/personal')"><span></span>我的</li>
     </ul>
 </template>
